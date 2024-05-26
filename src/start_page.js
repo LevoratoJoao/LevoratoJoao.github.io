e1bcb071beffd589c0f2ca4f706c15b9c3d8e47c
LevoratoJoao <j.vitor.jvlds@gmail.com>
2024-05-26 19:30:21 -0300

    Update: projects slider and responsive side bar

--- src/start_page.js
+++ src/start_page.js
@@ -12,3 +12,35 @@ textElement.addEventListener('animationend', function(event) {
         textElement.classList.add('no-cursor');
     }
 });
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    //slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+
+      950: {
+        slidesPerView: 3,
+      }
+    }
+  });
